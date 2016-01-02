d61b48a1324c75df1e28dcd8613c4751894d9807
21paradox <1036339815@qq.com>
2016-01-02 14:12:33 +0800

    ad cert

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var opt = {
 
     key: path.resolve('/etc/letsencrypt/live/www.21paradox.com/privkey.pem'),
     cert: path.resolve('/etc/letsencrypt/live/www.21paradox.com/cert.pem'),
+    ca: path.resolve('/etc/letsencrypt/live/www.21paradox.com/cert.pem'),
     port: 443
 
 };
 
 spdyProxy(opt);
-
