9f9385b854620c6c3663ad36921b3c1171421fb9
Shi Wenzhe <shiwenzhe@briphant.com>
2016-02-03 19:19:43 +0800

    add nconf as config

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var spdyProxy = require('./server.js').createServer;
 var path = require('path');
 var fs = require('fs');
+var nconf = require('nconf');
+
+// arguments confg -> environments conf -> config/config.json -> defaults
+nconf.argv()
+
+.env()
+
+.file({ file: path.join(__dirname, 'config.json')  });
+
+// setup defaults
+nconf.defaults({
+    port: 443
+});
 
 var opt = {
-    key: fs.readFileSync(path.resolve('/etc/letsencrypt/live/www.21paradox.com/privkey.pem')),
-    cert: fs.readFileSync(path.resolve('/etc/letsencrypt/live/www.21paradox.com/cert.pem')),
+    key: fs.readFileSync(nconf.get("keypath")),
+    cert: fs.readFileSync(nconf.get("certpath")),
     port: 443
 };
 
 var proxy = spdyProxy(opt);
-var setup = require('proxy');
+var proxySetup = require('proxy');
 
 var spdy = require('spdy');
 var server = spdy.createServer(opt);
@@ -24,9 +37,10 @@ var keepaliveAgent = new Agent({
 });
 
 server.agent = keepaliveAgent;
-var server1 = setup(server);
 
-server1.listen(443, function () {
+var spdyServer = proxySetup(server);
+
+spdyServer.listen(443, function () {
     var port = server.address().port;
     console.log('HTTP(s) proxy server listening on port %d', port);
 });
